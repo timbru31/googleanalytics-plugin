11a872750a42294c04657783bb857cc9edae94fa
xGhOsTkiLLeRx <ratchet.player@gmx.de>
2014-09-08 11:59:41 +0200

    Added: custom textbox for user input

--- src/main/java/hudson/plugins/google/analytics/GoogleAnalyticsPageDecorator.java
+++ src/main/java/hudson/plugins/google/analytics/GoogleAnalyticsPageDecorator.java
@@ -1,5 +1,8 @@
 package hudson.plugins.google.analytics;
 
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 import net.sf.json.JSONObject;
 
 import org.kohsuke.stapler.DataBoundConstructor;
@@ -14,13 +17,15 @@ public class GoogleAnalyticsPageDecorator extends PageDecorator {
     private String profileId;
     private boolean anonymizeIp;
     private boolean displayFeatures;
+    private ArrayList<String> customFeatures;
 
     @DataBoundConstructor
-    public GoogleAnalyticsPageDecorator(String profileId, boolean anonymizeIp, boolean displayFeatures) {
+    public GoogleAnalyticsPageDecorator(String profileId, boolean anonymizeIp, boolean displayFeatures, ArrayList<String> customFeatures) {
         this();
         this.profileId = profileId;
         this.anonymizeIp = anonymizeIp;
         this.displayFeatures = displayFeatures;
+        this.customFeatures = customFeatures;
     }
     
     public GoogleAnalyticsPageDecorator() {
@@ -63,4 +68,25 @@ public class GoogleAnalyticsPageDecorator extends PageDecorator {
     public void setDisplayFeatures(boolean displayFeatures) {
         this.displayFeatures = displayFeatures;
     }
+
+	public String getCustomFeatures() {
+		StringBuilder sb = new StringBuilder();
+		for (String s : this.customFeatures) {
+			sb.append(s);
+			sb.append("\n");
+		}
+		return sb.toString();
+	}
+
+	public void setCustomFeatures(ArrayList<String> customFeatures) {
+		this.customFeatures = customFeatures;
+	}
+	
+	public void setCustomFeatures(String rawCustomFeatures) {
+		this.customFeatures = new ArrayList<String>();
+		StringTokenizer tokenizer = new StringTokenizer(rawCustomFeatures, "\t\n\r\f");
+		while (tokenizer.hasMoreTokens()) {
+			this.customFeatures.add(tokenizer.nextToken());
+		}
+	}
 }
